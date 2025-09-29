64f435c5bae6404dd407662bfae65512512ad8e5
Aman Rathore <amanrathore01125@gmail.com>
2025-09-29 15:31:14 +0530

    rebuild frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -39,10 +39,6 @@ app.use(
 
 // React Router ke liye (Dashboard) - only for routes, not assets
 app.get("/dashboard/*", (req, res) => {
-  if (req.path.startsWith("/dashboard/assets")) {
-    // Agar assets ka request hai to static serve kare
-    return res.sendStatus(404); // Ye Express ko static serve karne dega
-  }
   res.sendFile(path.join(__dirname, "public/dashboard-dist", "index.html"));
 });
 
@@ -51,10 +47,6 @@ app.use("/", express.static(path.join(__dirname, "public/frontend-dist")));
 
 // React Router ke liye (Frontend)
 app.get("/*", (req, res) => {
-  if (req.path.startsWith("/assets")) {
-    // Agar assets ka request hai to static serve kare
-    return res.sendStatus(404);
-  }
   res.sendFile(path.join(__dirname, "public/frontend-dist", "index.html"));
 });
 
